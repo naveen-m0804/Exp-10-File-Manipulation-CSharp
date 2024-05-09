eefaf9a72060245d46dd008cf0b8cd6442fd6089
naveen-m0804 <mnaveenm2004@gmail.com>
2024-05-09 09:55:37 +0530

    First commit

--- Program.cs
+++ Program.cs
@@ -1,20 +1,4 @@
-# 19AI308-Object-Oriented-Programming-using-CSharp-Exp-10-File-Manipulation
-Develop a C# program to get the values from the user using structure and store it in a file in a specific path using file stream concept
-
-# AIM:
-To develop a C# program using file streams.
-
-# ALGORITHM:
-### Step 1: Define a structure UserData to hold user data fields like name, age, and email.
-### Step 2: Create an array of UserData to store multiple user data entries. Use a loop to prompt the user to input their name, age, and email, and store the input in the array.
-### Step 3: Specify the file path where the user data will be stored.
-### Step 4: Inside the loop, for each user data entry, open the file in append mode using StreamWriter and write the user data to the file.
-### Step 5: Close the file after writing all user data.
-### Step 6: Display a message indicating that the user data has been saved to the file.
-
-# PROGRAM:
-```
-using System;
+﻿using System;
 using System.IO;
 
 struct UserData
@@ -65,10 +49,3 @@ class Program
         }
     }
 }
-```
-
-# OUTPUT:
-![alt text](image.png)
-
-# RESULT:
-Thus the C# program to get the values from the user using structure and store it in a file in a specific path using file stream concept executed successfully.
